2036629afc81ba1f101e485cb805a609f5b48813
--get-all--gobal <kenneth.black@live.com>
2016-01-21 13:53:32 -0800

    modifying packages

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,23 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	maps = require('gulp-sourcemaps'),
 	del = require('del'),
-	vulcanize = require('vulcanize');
+	vulcanize = require('vulcanize'),
+	cdnizer = require('gulp-cdnizer');
 
 
 gulp.task('concatScripts', function() {
 	return gulp.src([])
 });
+
+gulp.task('cndizer', function() {
+	return gulp.src('./index.html')
+		.pipe(cdnizer({
+			allowRev: true,
+			allowMin: true,
+			files [
+				'google:angular',
+				'cdnjs:jquery'
+			]
+		}))
+		.pipe(gulp.dest('./dist'))
+})
