58248845c42c1d8e40fe69de38da47a5df821433
--get-all--gobal <kenneth.black@live.com>
2016-02-07 19:04:02 -0800

    Added 4 items to portfolio, optimized images

--- js/app.js
+++ js/app.js
@@ -20,14 +20,14 @@ angular.module('PortfolioApp', ['ngRoute'])
 	$scope.portfolio = [{
       title: 'Persona 4 Golden Guide',
       img: 'img/projects/persona.png',
-      skills: ['AngularJS', 'JavaScript', 'jQuery', 'AJAX', 'Nodejs', 'HTML5', 'CSS3', 'Photoshop'],
+      skills: ['AngularJS', 'JavaScript', 'jQuery', 'AJAX', 'Nodejs', 'Expressjs', 'HTML5', 'CSS3'],
       description: 'A website guide for Persona 4 Golden. I initially built this app using a node.js back end with express and swig, but down the line I realized that it would run much faster as an AngularJS single page application. The fullstack JavaScript version is saved as the \'node\' branch.',
       github: 'https://github.com/LawlietBlack/Persona-4-Golden-Guide',
       link: 'http://persona.lawlietblack.com'
     }, {
       title: 'Magic: the Gathering Deckbuilder',
       img: 'img/projects/mtg.png',
-      skills: ['AngularJS', 'Nodejs', 'Expressjs','RESTApi', 'MongoDB', 'Yeoman', 'Jasmine', 'TDD', 'HTML5', 'CSS3', 'JavaScript', 'AJAX'],
+      skills: ['AngularJS', 'Nodejs', 'Expressjs', 'RESTApi', 'MongoDB', 'Yeoman', 'Jasmine', 'TDD', 'HTML5', 'CSS3', 'JavaScript', 'AJAX'],
       description: 'Magic: the Gathering Deckbuilder that I built using MEAN stack.',
       github: 'https://github.com/LawlietBlack/magic-deckbuilder',
       link: 'http://magic-lawlietblack.herokuapp.com'
@@ -53,12 +53,40 @@ angular.module('PortfolioApp', ['ngRoute'])
       github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-15',
       link: 'http://codepen.io/LawlietBlack/full/JGBazL'
     }, {
+      title: 'Fallout Terminal Game',
+      img: 'img/projects/fallout.png',
+      skills: ['AngularJS', 'JavaScript', 'jQuery', 'HTML5', 'CSS3'],
+      description: 'Recreation of the hacking minigame in Fallout 4. Hosted on Codepen.',
+      github: 'https://github.com/LawlietBlack/fallout-terminal',
+      link: 'http://codepen.io/LawlietBlack/full/eJOZjN'
+    }, {
+      title: 'Exoplanet Heat Map',
+      img: 'img/projects/exoplanets.png',
+      skills: ['AngularJS', 'JavaScript', 'd3js', 'AJAX', 'HTML5', 'Sass'],
+      description: 'D3.js heat map comparing exoplanet radius, mass, and distance from Earth.',
+      github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-18',
+      link: 'http://codepen.io/LawlietBlack/full/wMEbBV/'
+    }, {
       title: 'Conway\'s Game of Life',
       img: 'img/projects/life.png',
       skills: ['ReactJS', 'JavaScript', 'Sass', 'HTML5'],
       description: 'Conway\'s game of life with grid size, generation speed, pause/play, and randomize functionality. Hosted on Codepen.',
       github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-14',
       link: 'http://codepen.io/LawlietBlack/full/oboRwo/'
+    }, {
+      title: 'Camper News Force Map',
+      img: 'img/projects/camperforce.png',
+      skills: ['AngularJS', 'JavaScript', 'd3js', 'AJAX', 'HTML5', 'Sass'],
+      description: 'FreeCodeCamp\'s news feed visualized using a d3 force map. Hosted on Codepen.',
+      github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-19',
+      link: 'http://codepen.io/LawlietBlack/full/YwJebb/'
+    }, {
+      title: 'US Gross National Product',
+      img: 'img/projects/usgnp.png',
+      skills: ['AngularJS', 'JavaScript', 'd3js', 'AJAX', 'HTML5', 'Sass'],
+      description: 'D3.js chart of the United States Gross National Product from 1947 to present. Hosted on Codepen',
+      github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-16',
+      link: 'http://codepen.io/LawlietBlack/full/bExgVR/'
     }, {
       title: 'Recipe Box',
       img: 'img/projects/recipebox.png',
@@ -69,7 +97,7 @@ angular.module('PortfolioApp', ['ngRoute'])
     }, {
       title: 'Vote',
       img: 'img/projects/vote.png',
-      skills: [ 'AngularJS', 'Nodejs','Expressjs', 'JavaScript', 'MongoDB', 'Yeoman', 'oAuth',  'HTML5', 'AJAX', 'CSS3', 'Bootstrap'],
+      skills: [ 'AngularJS', 'Nodejs','Expressjs', 'JavaScript', 'MongoDB', 'RESTApi', 'Yeoman', 'oAuth',  'HTML5', 'AJAX', 'CSS3', 'Bootstrap'],
       description: 'Voting app built using MEAN stack and oAuth that allows users to login, create polls, and share them with others.',
       github: 'https://github.com/LawlietBlack/vote',
       link: 'http://vote-lawlietblack.herokuapp.com'
@@ -81,23 +109,30 @@ angular.module('PortfolioApp', ['ngRoute'])
       github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-11',
       link: 'http://codepen.io/LawlietBlack/full/vLgmdm'
     }, {
+      title: 'FreeCodeCamp Leaderboard',
+      img: 'img/projects/fccleaders.png',
+      skills: ['ReactJS', 'JavaScript', 'AJAX', 'Sass', 'HTML5', 'Responsive'],
+      description: 'Leaderboard for FreeCodeCamp.com created with ReactJS and Sass. Utilizes an api endpoint made by a fellow student. Hosted on Codepen.',
+      github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-12',
+      link: 'http://codepen.io/LawlietBlack/full/rxjZEq'
+    }, {
       title: 'Stocks',
       img: 'img/projects/stocks.png',
-      skills: [ 'AngularJS', 'Nodejs', 'd3js', 'Expressjs', 'MongoDB', 'Socketio', 'Yeoman', 'HTML5', 'AJAX', 'CSS3', 'Bootstrap', 'JavaScript'],
+      skills: [ 'AngularJS', 'Nodejs', 'd3js', 'Expressjs', 'MongoDB', 'RESTApi', 'Socketio', 'Yeoman', 'HTML5', 'AJAX', 'CSS3', 'Bootstrap', 'JavaScript'],
       description: 'Single Page App that charts the stock market using data from the Markit on demand Market Data API. Chart updates for all users in real time whenever a stock is added or removed.',
       github: 'https://github.com/LawlietBlack/stocks',
       link: 'http://stocks-lawlietblack.herokuapp.com'
     }, {
       title: 'Nightlife Coordinator',
       img: 'img/projects/nightlife.png',
-      skills: ['AngularJS', 'Nodejs', 'JavaScript', 'Expressjs', 'MongoDB', 'Yeoman', 'oAuth', 'HTML5', 'AJAX', 'CSS3', 'Bootstrap'],
+      skills: ['AngularJS', 'Nodejs', 'JavaScript', 'Expressjs', 'MongoDB', 'RESTApi', 'Yeoman', 'oAuth', 'HTML5', 'AJAX', 'CSS3', 'Bootstrap'],
       description: 'Single Page App that allows the user to search for local bars or restraunts and shows locations and ratings using the Yelp API. Users can then mark where they are going that night so they can coordinate with their friends.',
       github: 'https://github.com/LawlietBlack/nightlife',
       link: 'http://nightlife-lawlietblack.herokuapp.com'
     }, {
       title: 'Book Trade',
       img: 'img/projects/booktrade.png',
-      skills: ['AngularJS', 'Nodejs', 'JavaScript', 'Expressjs', 'MongoDB', 'Yeoman', 'oAuth', 'HTML5', 'AJAX', 'CSS3', 'Bootstrap'],
+      skills: ['AngularJS', 'Nodejs', 'JavaScript', 'Expressjs', 'MongoDB', 'RESTApi', 'Yeoman', 'oAuth', 'HTML5', 'AJAX', 'CSS3', 'Bootstrap'],
       description: 'Prototype Book Trading site for a local club. Uses the Google Books API and a RESTful API to allow users to search, select, and trade books. Upon successful trade, enables contact info trading (limited). ',
       github: 'https://github.com/LawlietBlack/books',
       link: 'http://books-lawlietblack.herokuapp.com'
@@ -129,13 +164,6 @@ angular.module('PortfolioApp', ['ngRoute'])
       description: 'Simon game recreated in HTML and JavaScript. Supports normal and Strict mode, increases tempo at turn 5, 9, and 13. Player win at turn 20. Hosted on Codepen.',
       github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-10',
       link: 'http://codepen.io/LawlietBlack/full/RWWzLz'
-    }, {
-      title: 'FreeCodeCamp Leaderboard',
-      img: 'img/projects/fccleaders.png',
-      skills: ['ReactJS', 'JavaScript', 'AJAX', 'Sass', 'HTML5', 'Responsive'],
-      description: 'Leaderboard for FreeCodeCamp.com created with ReactJS and Sass. Utilizes an api endpoint made by a fellow student. Hosted on Codepen.',
-      github: 'https://github.com/LawlietBlack/FCC-Ziplines/tree/master/Zipline-12',
-      link: 'http://codepen.io/LawlietBlack/full/rxjZEq'
     }]
 
     // }, {
@@ -154,7 +182,7 @@ angular.module('PortfolioApp', ['ngRoute'])
     }).reduce(function(a, b) {
       return a.concat(b);
     });
-    $scope.skillList = ["AngularJS", "JavaScript", "jQuery", "Nodejs", "Expressjs", "ReactJS", "d3js", "AJAX", "JSX", "MongoDB", "Yeoman", "oAuth", "Jasmine", "TDD", "RESTApi", "Babel", "HTML5", "CSS3", "Bootstrap", "Sass", "Responsive", "Bourbon", "Photoshop"]
+    $scope.skillList = ["AngularJS", "JavaScript", "jQuery", "Nodejs", "Expressjs", "ReactJS", "d3js", "AJAX", "JSX", "MongoDB", "Yeoman", "oAuth", "Jasmine", "TDD", "RESTApi", "Babel", "HTML5", "CSS3", "Bootstrap", "Sass", "Responsive", "Bourbon"]
     for(var i=0; i<skillsData.length; i++) {
       if($scope.skills[skillsData[i]] === undefined) {
         $scope.skills[skillsData[i]] = 1;
